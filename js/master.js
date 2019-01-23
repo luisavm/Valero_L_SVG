05acf325655b5a41317fdac9b52a96e0e5122899
Luisa Valero <luisa.valero19@gmail.com>
2019-01-23 14:25:18 -0500

    Fixed some errors on line 22

--- js/master.js
+++ js/master.js
@@ -19,6 +19,13 @@
     // 1 to 1 event handlling
 	badge.addEventListener("click", logMyId);
 
-	allTheBadges.forEach(currentbandge => currentbandge.addEventListener('click, logMyId'));
+	allTheBadges.forEach(currentbandge => currentbandge.addEventListener("click", logMyId));
+
+	// currentbandge is just a placeholder... you could call it anything. It
+	// refers to the current item in the collection
+
+	// so we need to debug this line by line in the developer tools panel
+
+	// the lst line (22) need a quote mark after click, not at the end. JavaScript can be so frustrating because one little thing out of place breaks  EVERYTHING
 
 })();
